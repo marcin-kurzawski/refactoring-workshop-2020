93427755975d40a13e632617dd8473999e832c7f
marcin-kurzawski <kurzawski.mk@gmail.com>
2020-10-22 18:17:48 +0200

    testchallenge

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -192,4 +192,60 @@ void Controller::receive(std::unique_ptr<Event> e)
     }
 }
 
+/*
+todolista:
+-rozbic bloba i wydzielic mu funkcje + wyodrebnic bloki z try i catch
+
+
+
+
+
+
+//first
+if (not lost) {
+            if (std::make_pair(newHead.x, newHead.y) == m_foodPosition) {
+                m_scorePort.send(std::make_unique<EventT<ScoreInd>>());
+                m_foodPort.send(std::make_unique<EventT<FoodReq>>());
+            } else if (newHead.x < 0 or newHead.y < 0 or
+                       newHead.x >= m_mapDimension.first or
+                       newHead.y >= m_mapDimension.second) {
+                m_scorePort.send(std::make_unique<EventT<LooseInd>>());
+                lost = true;
+            } else {
+                for (auto &segment : m_segments) {
+                    if (not --segment.ttl) {
+                        DisplayInd l_evt;
+                        l_evt.x = segment.x;
+                        l_evt.y = segment.y;
+                        l_evt.value = Cell_FREE;
+
+                        m_displayPort.send(std::make_unique<EventT<DisplayInd>>(l_evt));
+                    }
+                }
+            }
+        }
+
+
+
+
+
+//second
+        if (not lost) {
+            m_segments.push_front(newHead);
+            DisplayInd placeNewHead;
+            placeNewHead.x = newHead.x;
+            placeNewHead.y = newHead.y;
+            placeNewHead.value = Cell_SNAKE;
+
+            m_displayPort.send(std::make_unique<EventT<DisplayInd>>(placeNewHead));
+
+            m_segments.erase(
+                std::remove_if(
+                    m_segments.begin(),
+                    m_segments.end(),
+                    [](auto const& segment){ return not (segment.ttl > 0); }),
+                m_segments.end());
+        }
+*/
+
 } // namespace Snake
